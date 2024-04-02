64c190fa4238ddfe9ace6884724af3cecdffd077
Amit Biswas <amit.142biswas@gmail.com>
2024-04-02 17:09:04 +0530

    next-auth, prisma, supabase added

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,4 @@
+import NextAuth from "next-auth/next";
+
+const handler = NextAuth()
+
---
+++ app/utils/auth.ts
@@ -0,0 +1,10 @@
+import type { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./db";
+
+export const authOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+
+    ]
+} satisfies NextAuthOptions
