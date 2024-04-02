7b2101e9117aaa2f89ee4acf69f313c9ccc2d2e7
Amit Biswas <amit.142biswas@gmail.com>
2024-04-02 21:53:30 +0530

    github auth provided

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,10 +1,13 @@
 import type { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./db";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-
-    ]
-} satisfies NextAuthOptions
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+} satisfies NextAuthOptions;
