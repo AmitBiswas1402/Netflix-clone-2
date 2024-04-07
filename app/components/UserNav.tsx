af9a2d4eee2d8eb3d4fbe521ed89eeaf796186c4
Amit Biswas <amit.142biswas@gmail.com>
2024-04-07 13:36:26 +0530

    sucks

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -1,19 +1,42 @@
-import { AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
-import { Avatar } from "@radix-ui/react-avatar"
-import { DropdownMenu, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Avatar } from "@radix-ui/react-avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu";
+import { signOut } from "next-auth/react";
 
 export default function UseNav() {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Button variant="ghost" className="relative h-10 w-10 rounded-sm"> 
-                <Avatar className="h-10 w-10 rounded-sm">
-                    <AvatarImage src="https://majemcyhbjxnrrwyxnph.supabase.co/storage/v1/object/public/Amit%20image/avatar.png" />
-                    <AvatarFallback className="rounded-sm">Jan</AvatarFallback>
-                </Avatar>
-                </Button>
-            </DropdownMenuTrigger>
-        </DropdownMenu>
-    )
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button variant="ghost" className="relative h-10 w-10 rounded-sm">
+          <Avatar className="h-10 w-10 rounded-sm">
+            <AvatarImage src="https://majemcyhbjxnrrwyxnph.supabase.co/storage/v1/object/public/Amit%20image/avatar.png" />
+            <AvatarFallback className="rounded-sm">Jan</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel>
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">Jan</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              jkasdf@asdkfj.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>
+            Sign Out 
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
